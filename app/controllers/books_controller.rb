3c9f301ddb7fc5174a82ff716675a860d6b70b92
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-02 02:16:53 +0000

    [Add]Rubocop install

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,24 @@
 class BooksController < ApplicationController
   protect_from_forgery
-  
+
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-  if @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
-  else
-    @books = Book.all
-    @user = current_user
-    render :index
-  end
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
   end
 
   def index
     @book = Book.new
     @books = Book.all
     @user = current_user
-
   end
 
   def show
@@ -31,20 +30,20 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     @user = current_user
-  if @book.user == current_user
-  else 
-     redirect_to books_path
-  end 
+    if @book.user == current_user
+    else
+      redirect_to books_path
+    end
   end
-  
+
   def update
     @book = Book.find(params[:id])
-  if @book.update(book_params)
-    flash[:notice] = "You have updated book successfully."
-    redirect_to book_path(@book.id)
-  else
-    render :edit
-  end 
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -54,9 +53,7 @@ class BooksController < ApplicationController
   end
 
     private
-
-  def book_params
-     params.require(:book).permit(:title,:body)
-  end
-  
+      def book_params
+        params.require(:book).permit(:title, :body)
+      end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-
-  before_action :correct_user,only:[:edit,:update]
+  before_action :correct_user, only: [:edit, :update]
   before_action :ensure_guest_user, only: [:edit]
 
   def index
@@ -20,30 +19,29 @@ class UsersController < ApplicationController
   end
 
   def update
-     @user = User.find(params[:id])
-  if @user.update(user_params)
-     flash[:notice] = "You have updated user successfully."
-     redirect_to user_path(id: current_user)
-  else
-     render :edit
-  end
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(id: current_user)
+    else
+      render :edit
+    end
   end
 
     private
-
-  def user_params
-    params.require(:user).permit(:name,:introduction,:profile_image)
-  end
-  
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to user_path(current_user.id) unless @user == current_user
-  end
-  
-    def ensure_guest_user
-      @user = User.find(params[:id])
-      if @user.name == "guestuser"
-        redirect_to user_path(current_user) , notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+      def user_params
+        params.require(:user).permit(:name, :introduction, :profile_image)
       end
-    end  
+
+      def correct_user
+        @user = User.find(params[:id])
+        redirect_to user_path(current_user.id) unless @user == current_user
+      end
+
+      def ensure_guest_user
+        @user = User.find(params[:id])
+        if @user.name == "guestuser"
+          redirect_to user_path(current_user), notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+        end
+      end
 end
