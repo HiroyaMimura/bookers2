15983fc3e0f5d8c0256a2ea81891ba50172d3d2b
HiroyaMimura <113226191+HiroyaMimura@users.noreply.github.com>
2022-11-02 01:58:12 +0000

    [Add]ゲストログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
   resources :users,only:[:index,:show,:edit,:update]
   resources :books,only:[:index,:create,:show,:edit,:update,:destroy]
- 
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
